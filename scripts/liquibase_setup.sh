51220799c0d9f3bd1c09840962278eae597e7b1f
Mark Mixson <mark.mixson@ossys.com>
2017-08-22 00:10:56 -0400

    updated to use latest liquibase and mariadb connector

--- scripts/liquibase_setup.sh
+++ scripts/liquibase_setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 echo "Setting up liquibase"
-: ${DB_ENV_POSTGRES_USER?"DB_ENV_POSTGRES_USER not set"}
-: ${DB_ENV_POSTGRES_PASSWORD?"DB_ENV_POSTGRES_PASSWORD not set"}
 
 cat <<CONF > /liquibase.properties
-  driver: org.postgresql.Driver
-  classpath:/usr/local/bin/postgresql-9.3-1102.jdbc41.jar
-  url: jdbc:postgresql://$DB_PORT_5432_TCP_ADDR:$DB_PORT_5432_TCP_PORT/$POSTGRES_USER
-  username: $DB_ENV_POSTGRES_USER
-  password: $DB_ENV_POSTGRES_PASSWORD
+  driver: org.mariadb.jdbc.Driver
+  classpath:/usr/local/bin/mariadb-java-client-2.1.0.jar
+  url: jdbc:mariadb://$DB_IP:3306/$DB_NAME
+  username: $DB_USER
+  password: $DB_PASS
 CONF
