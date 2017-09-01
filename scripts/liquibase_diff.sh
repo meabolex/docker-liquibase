26427b03f751f155e1500a9af7d4ef8b166d67ed
Mark Mixson <mark.mixson@ossys.com>
2017-08-31 21:49:53 -0400

    updated file naming scheme

--- scripts/liquibase_diff.sh
+++ scripts/liquibase_diff.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-: ${CHANGELOG_FILE:="changelog_diff.xml"}
 : ${CONNECTION_STRING? "please provide the connection string of the base database"}
 : ${DB_USER?"Please provide the database user name"}
 : ${DB_PASS?"Please provide the database password"}
@@ -7,20 +6,20 @@
 
 TS=$(date +%s)
 echo "Generating diff ...."
-liquibase --changeLogFile="/changelogs/diff/$TS-$CHANGELOG_FILE" diffChangeLog \
+liquibase --changeLogFile="/changelogs/diff/$TS-changelog_diff.xml" diffChangeLog \
   --referenceUrl=$CONNECTION_STRING \
   --referenceUsername=$DB_USER \
   --referencePassword=$DB_PASS \
   -Dappuser_password="$APPUSER_PASSWORD"
 
-echo "Diff generated into: /changelogs/diff/$TS-$CHANGELOG_FILE"
+echo "Diff generated into: /changelogs/diff/$TS-changelog_diff.xml"
 
 if [ ! -z "$LIQUIBASE_INCLUSION_FILE" ] ; then
   if [ -f "/changelogs/$LIQUIBASE_INCLUSION_FILE" ] ; then
     echo "Include newly generated file into the list of changesets";
     sed -i "
       /<\/databaseChangeLog>/ i\
-      <include relativeToChangelogFile='true' file=\"/diff/$TS-$CHANGELOG_FILE\" />" "/changelogs/$LIQUIBASE_INCLUSION_FILE"
+      <include relativeToChangelogFile='true' file=\"/diff/$TS-changelog_diff.xml\" />" "/changelogs/$LIQUIBASE_INCLUSION_FILE"
   else
     echo "Liquibase include file doesn't exist: /changelogs/$LIQUIBASE_INCLUSION_FILE"
   fi
--- scripts/liquibase_generate.sh
+++ scripts/liquibase_generate.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 : ${APPUSER_PASSWORD?"Please enter the password for the app user"}
-: ${CHANGELOG_FILE:="changelog_diff.xml"}
-
 
 TS=$(date +%s)
 echo "Generating changelog ...."
-liquibase --diffTypes="$DIFF_TYPES" --defaultSchemaName="$DB_SCHEMA_NAME" --changeLogFile="/changelogs/$TS-$CHANGELOG_FILE" generateChangeLog -Dappuser_password="$APPUSER_PASSWORD"
+liquibase --diffTypes="$DIFF_TYPES" --defaultSchemaName="$DB_SCHEMA_NAME" --changeLogFile="/changelogs/$TS-changelog_diff.xml" generateChangeLog -Dappuser_password="$APPUSER_PASSWORD"
 
-echo "Changelog generated into: /changelogs/$TS-$CHANGELOG_FILE"
+echo "Changelog generated into: /changelogs/$TS-changelog_diff.xml"
