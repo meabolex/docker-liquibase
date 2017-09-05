4313c1f6aeece61e340035abd9d670e598bcade6
Mark Mixson <mark.mixson@ossys.com>
2017-09-04 23:04:42 -0400

    changed app password to use app password hash

--- scripts/liquibase_generate.sh
+++ scripts/liquibase_generate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-: ${APPUSER_PASSWORD?"Please enter the password for the app user"}
+: ${APPUSER_PASSWORD_HASH?"Please enter the password hash for the app user"}
 : ${CHANGELOG_FILE:="changelog_diff.xml"}
 
 TS=$(date +%s)
 echo "Generating changelog ...."
-liquibase --diffTypes="$DIFF_TYPES" --defaultSchemaName="$DB_SCHEMA_NAME" --changeLogFile="/changelogs/$TS-$CHANGELOG_FILE" generateChangeLog -Dappuser_password="$APPUSER_PASSWORD"
+liquibase --diffTypes="$DIFF_TYPES" --defaultSchemaName="$DB_SCHEMA_NAME" --changeLogFile="/changelogs/$TS-$CHANGELOG_FILE" generateChangeLog -Dappuser_password_hash="$APPUSER_PASSWORD_HASH"
 
 echo "Changelog generated into: /changelogs/$TS-$CHANGELOG_FILE"
--- scripts/liquibase_update.sh
+++ scripts/liquibase_update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-: ${APPUSER_PASSWORD?"Please enter the password for the app user"}
+: ${APPUSER_PASSWORD?"Please enter the password hash for the app user"}
 : ${CHANGELOG_FILE:="changelogs.xml"}
 
 echo "Applying changes to the database. Changelog: $CHANGELOG_FILE"
-liquibase --changeLogFile="/changelogs/$CHANGELOG_FILE" update -Dappuser_password="$APPUSER_PASSWORD"
+liquibase --changeLogFile="/changelogs/$CHANGELOG_FILE" update -Dappuser_password_hash="$APPUSER_PASSWORD_HASH"
