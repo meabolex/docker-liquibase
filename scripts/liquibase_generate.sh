874f4bd343dafea4f8968711e43d08165991f0ed
Mark Mixson <mark.mixson@ossys.com>
2017-09-05 00:32:39 -0400

    added more information passed to changelog variables

--- scripts/liquibase_generate.sh
+++ scripts/liquibase_generate.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 : ${APPUSER_PASSWORD_HASH?"Please enter the password hash for the app user"}
+: ${APPUSER_NAME?"Please enter the user name for the app user"}
 : ${CHANGELOG_FILE:="changelog_diff.xml"}
+: ${DB_NAME?"Please enter the database name"}
 
 TS=$(date +%s)
 echo "Generating changelog ...."
-liquibase --diffTypes="$DIFF_TYPES" --defaultSchemaName="$DB_SCHEMA_NAME" --changeLogFile="/changelogs/$TS-$CHANGELOG_FILE" generateChangeLog -Dappuser_password_hash="$APPUSER_PASSWORD_HASH"
+liquibase --diffTypes="$DIFF_TYPES" --defaultSchemaName="$DB_SCHEMA_NAME" --changeLogFile="/changelogs/$TS-$CHANGELOG_FILE" generateChangeLog -Dappuser_password_hash="$APPUSER_PASSWORD_HASH" -Dappuser_username="$APPUSER_NAME" -Ddb_name="$DB_NAME"
 
 echo "Changelog generated into: /changelogs/$TS-$CHANGELOG_FILE"
--- scripts/liquibase_update.sh
+++ scripts/liquibase_update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 : ${APPUSER_PASSWORD_HASH?"Please enter the password hash for the app user"}
+: ${APPUSER_NAME?"Please enter the user name for the app user"}
 : ${CHANGELOG_FILE:="changelogs.xml"}
+: ${DB_NAME?"Please enter the database name"}
 
 echo "Applying changes to the database. Changelog: $CHANGELOG_FILE"
-liquibase --changeLogFile="/changelogs/$CHANGELOG_FILE" update -Dappuser_password_hash="$APPUSER_PASSWORD_HASH"
+liquibase --changeLogFile="/changelogs/$CHANGELOG_FILE" update -Dappuser_password_hash="$APPUSER_PASSWORD_HASH" -Dappuser_username="$APPUSER_NAME" -Ddb_name="$DB_NAME"
